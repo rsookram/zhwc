5cb7c01368a9be0a1641198730af9e5e95a8a9da
rsookram <rashadsookram@gmail.com>
2022-05-27 21:22:07 -0400

    Read files and segment in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use jieba_rs::Jieba;
 use pico_args::Arguments;
 use std::collections::{HashMap, HashSet};
@@ -7,6 +7,8 @@ use std::fs;
 use std::io::BufWriter;
 use std::io::Write;
 use std::path::PathBuf;
+use std::sync::Arc;
+use std::thread;
 
 fn main() -> Result<()> {
     let mut args = Arguments::from_env();
@@ -28,17 +30,36 @@ fn main() -> Result<()> {
         None => Default::default(),
     };
 
-    let jieba = Jieba::new();
+    let jieba = Arc::new(Jieba::new());
 
     let mut counts = HashMap::<_, u32>::with_capacity(8 * 1024);
 
-    for path in paths {
-        let text = fs::read_to_string(&path)?;
+    let mut handles = Vec::new();
 
-        let words = jieba.cut(&text, true);
+    for path in paths {
+        // TODO: Limit parallelism
+        let jieba = Arc::clone(&jieba);
+
+        handles.push(thread::spawn(move || {
+            fs::read_to_string(&path).map(|text| {
+                jieba
+                    .cut(&text, true)
+                    .into_iter()
+                    .map(|w| w.to_string())
+                    .collect::<Vec<_>>()
+            })
+        }));
+    }
+
+    for handle in handles {
+        let words = match handle.join() {
+            Ok(r) => r?,
+            Err(e) => bail!("{e:?}"),
+        };
+
         for w in words {
-            if should_count(&excludes, w) {
-                *counts.entry(w.to_string()).or_insert(0) += 1;
+            if should_count(&excludes, &w) {
+                *counts.entry(w).or_insert(0) += 1;
             }
         }
     }
